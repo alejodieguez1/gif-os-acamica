8545153fc23c0d934d61915c94192ab6c560f329
Alejo Dieguez <70345593+alejodieguez1@users.noreply.github.com>
2021-05-31 08:16:50 -0300

    added js darkMode event

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,12 @@
 var darkMode = document.getElementById("dark-mode");
+var topBar = document.getElementById("top-bar");
+
 darkMode.addEventListener("click", () => {
-    var topBar = document.getElementsByClassName("top-bar");
-    topBar.classList.add("dark-mode")
-})
+    if (darkMode.textContent === 'Modo Nocturno')
+    darkMode.textContent = 'Modo Diurno';
+    else darkMode.textContent = 'Modo Nocturno';
+
+    if (darkMode.textContent === "Modo Diurno")
+    topBar.classList.replace("top-bar", "darkMode");
+    else topBar.classList.replace("darkMode", "top-bar");
+});
