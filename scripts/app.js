3f9581d1f4feac74f16c0aa7e591c5f6924c7e7d
Alejo Dieguez <70345593+alejodieguez1@users.noreply.github.com>
2021-06-03 07:30:16 -0300

    darkmode adds

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,7 @@
 var darkMode = document.getElementById("dark-mode");
 var topBar = document.getElementById("top-bar");
 var bodyContainer = document.querySelector("body");
+var headerContainer = document.getElementById("header-container");
 
 darkMode.addEventListener("click", () => {
     if (darkMode.textContent === 'Modo Nocturno')
@@ -8,10 +9,14 @@ darkMode.addEventListener("click", () => {
     else darkMode.textContent = 'Modo Nocturno';
 
     if (darkMode.textContent === "Modo Diurno")
+    headerContainer.style.backgroundColor = ("#37383C");
+    else headerContainer.style.backgroundColor = ("#ffff")
+
+    if (darkMode.textContent === "Modo Diurno")
     topBar.classList.replace("top-bar", "darkMode");
     else topBar.classList.replace("darkMode", "top-bar");
 
-    if(darkMode.textContent === "Modo Diurno")
+    if (darkMode.textContent === "Modo Diurno")
     bodyContainer.style.backgroundColor = ("#37383C");
     else bodyContainer.style.backgroundColor = ("white");
 });
