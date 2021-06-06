53331d8fbbbfaf40262dc72f5b14cbfbfa058a88
Alejo Dieguez <70345593+alejodieguez1@users.noreply.github.com>
2021-06-06 20:57:32 -0300

    darkMode mobile complete

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,23 @@
 var darkMode = document.getElementById("dark-mode");
 var bodyContainer = document.getElementById("bodyContainer");
+var textLogo = document.querySelector("#GIFOS_1");
+var searchIcon = document.getElementById("search-icon");
 
 darkMode.addEventListener("click", () => {
     if (darkMode.textContent === 'Modo Nocturno')
     darkMode.textContent = 'Modo Diurno';
     else darkMode.textContent = 'Modo Nocturno';
 
-    // if (darkMode.textContent === "Modo Diurno")
-    // bodyContainer.classList.replace("body-container","darkMode");
-    // else bodyContainer.classList.remove("darkMode", "body-container")
+    if (darkMode.textContent === "Modo Diurno")
+    bodyContainer.classList.replace("body-container","darkMode");
+    else bodyContainer.classList.replace("darkMode", "body-container")
+
+    if (bodyContainer.classList.contains("darkMode"))
+    textLogo.setAttribute("fill", "#ffff");
+    else textLogo.setAttribute("fill", "#572EE5")
+
+    if (bodyContainer.classList.contains("darkMode"))
+    searchIcon.src = "/images/icon-search-modo-noct.svg";
+    else searchIcon.src = "/images/icon-search.svg";
+
 });
