61cbb2433b1d8cd0a1da34268bdb73f11baf1c8a
Alejo Dieguez <70345593+alejodieguez1@users.noreply.github.com>
2021-06-06 21:08:35 -0300

    Responsive dark mode working

--- scripts/app.js
+++ scripts/app.js
@@ -2,6 +2,7 @@ var darkMode = document.getElementById("dark-mode");
 var bodyContainer = document.getElementById("bodyContainer");
 var textLogo = document.querySelector("#GIFOS_1");
 var searchIcon = document.getElementById("search-icon");
+var createBtn = document.getElementById("create-gif");
 
 darkMode.addEventListener("click", () => {
     if (darkMode.textContent === 'Modo Nocturno')
@@ -16,8 +17,11 @@ darkMode.addEventListener("click", () => {
     textLogo.setAttribute("fill", "#ffff");
     else textLogo.setAttribute("fill", "#572EE5")
 
-    if (bodyContainer.classList.contains("darkMode"))
-    searchIcon.src = "/images/icon-search-modo-noct.svg";
-    else searchIcon.src = "/images/icon-search.svg";
+    if (bodyContainer.classList.contains("darkMode")) {
+        searchIcon.src = "/images/icon-search-modo-noct.svg";
+    }else searchIcon.src = "/images/icon-search.svg";
 
+    if (bodyContainer.classList.contains("darkMode")) {
+        createBtn.src = "/images/CTA-crear-gifo-modo-noc.svg";
+    }else createBtn.src = "/images/button-crear-gifo.svg";
 });
