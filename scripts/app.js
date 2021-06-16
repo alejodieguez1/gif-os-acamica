35bb95cc3cb0acd26381e2dd687dad5a0e2eeb83
Alejo Dieguez <70345593+alejodieguez1@users.noreply.github.com>
2021-06-16 08:08:15 -0300

    api integration testing

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,18 @@
 const apiKey = "p8ZMZYpVy2Zl7HlxVPCToc461j1rDepR";
 const url = "https://api.giphy.com/v1";
+const urlTrending = `${url}/gifs/trending?api_key=${apiKey}`
+//API REQUEST FUNCTION
+
+// function request(url) {
+//     return new Promise((resolve, reject) => {
+//       fetch(url)
+//         .then((response) => response.json())
+//         .then((response) => {
+//           return resolve(response)
+//         })
+//         .catch((error) => reject(error))
+//     })
+//   }
+fetch(urlTrending)
+.then(res => res.json())
+.then(data => console.log(data))
