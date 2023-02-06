029b4e12331537dc7b50c8618a7561b2c03b4d52
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-02-06 16:33:51 -0300

    feat: fetch api finally working

--- scripts/app.js
+++ scripts/app.js
@@ -13,6 +13,28 @@ function request(url) {
       .catch((error) => reject(error));
   });
 }
-fetch(urlTrending)
-  .then((res) => res.json())
-  .then((data) => console.log(data.data[0]));
+
+//Gif card creation function
+function createItem(src, container, itemId) {
+  const item = document.createElement("div");
+  item.className = itemId;
+  const gif = document.createElement("img");
+  gif.src = src;
+  item.appendChild(gif);
+  container.appendChild(item);
+}
+
+request(urlTrending)
+  .then((data) => {
+    const contenedor = document.querySelector("#trending-gifos");
+    for (let i = 0; i <= 10; i++) {
+      createItem(
+        data.data[i].images.downsized.url,
+        contenedor,
+        "gif-container"
+      );
+    }
+  })
+  .catch((error) => {
+    console.error(error);
+  });
