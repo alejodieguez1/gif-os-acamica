907224aace4c595664ba6879dc5855b9b1a8061b
Alejo Dieguez <alejodieguez2012@gmail.com>
2023-02-13 19:13:59 -0300

    feature: improved darkMode functionality
    added a checkbox hack for the dark mode toggle

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,23 +1,36 @@
-const darkMode = document.getElementById("dark-mode");
+const darkMode = document.getElementById("toggleDark");
+const labelDark = document.querySelector(".labelDark");
 const bodyContainer = document.getElementById("bodyContainer");
 const textLogo = document.querySelector("#GIFOS_1");
 const searchIcon = document.getElementById("search-icon");
 const createBtn = document.getElementById("create-gif");
 const searchInput = document.getElementById("txt-search");
 
-darkMode.addEventListener("click", () => {
+// if (bodyContainer.classList.contains("darkMode")) {
+//   createBtn.src = "../images/CTA-crear-gifo-modo-noc.svg";
+//   createBtn.addEventListener("mouseenter", () => {
+//     createBtn.src = "../images/CTA-crear-gifo-hover-modo-noc.svg";
+//   });
+//   createBtn.addEventListener("mouseleave", () => {
+//     createBtn.src = "../images/CTA-crear-gifo-modo-noc.svg";
+//   });
+// }
+
+// if (bodyContainer.classList.contains("body-container")) {
+//   createBtn.src = "../images/button-crear-gifo.svg";
+//   createBtn.addEventListener("mouseenter", () => {
+//     createBtn.src = "../images/CTA-crear-gifo-hover.svg";
+//   });
+//   createBtn.addEventListener("mouseleave", () => {
+//     createBtn.src = "../images/button-crear-gifo.svg";
+//   });
+// }
+
+darkMode.addEventListener("change", () => {
   if (bodyContainer.classList.contains("body-container")) {
     bodyContainer.classList.replace("body-container", "darkMode");
-    darkMode.textContent = "Modo Diurno";
+    labelDark.innerHTML = "Modo Diurno";
     textLogo.setAttribute("fill", "#ffff");
-
-    createBtn.src = "../images/CTA-crear-gifo-modo-noc.svg";
-    createBtn.addEventListener("mouseenter", () => {
-      createBtn.src = "../images/CTA-crear-gifo-hover-modo-noc.svg";
-    });
-    createBtn.addEventListener("mouseleave", () => {
-      createBtn.src = "../images/CTA-crear-gifo-modo-noc.svg";
-    });
 
     searchIcon.src = "/images/icon-search-modo-noct.svg";
     searchInput.addEventListener("keyup", () => {
@@ -30,15 +43,7 @@ darkMode.addEventListener("click", () => {
   } else {
     textLogo.setAttribute("fill", "#572EE5");
     bodyContainer.classList.replace("darkMode", "body-container");
-    darkMode.textContent = "Modo Nocturno";
-
-    createBtn.src = "../images/button-crear-gifo.svg";
-    createBtn.addEventListener("mouseenter", () => {
-      createBtn.src = "../images/CTA-crear-gifo-hover.svg";
-    });
-    createBtn.addEventListener("mouseleave", () => {
-      createBtn.src = "../images/button-crear-gifo.svg";
-    });
+    labelDark.innerHTML = "Modo Nocturno";
 
     searchIcon.src = "../images/icon-search.svg";
     searchInput.addEventListener("keyup", () => {
